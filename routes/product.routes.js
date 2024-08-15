b1e466ef3d9d9bda885850e2376737b5bbb25b8c
sf-murshid <farhan.murshid.sf@gmail.com>
2024-08-16 03:20:06 +0530

    modify product controllers(routes)

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -35,7 +35,12 @@ route.post('/', async(req, res) => {
 })
 
 route.get('/', async(req,res) => {
-    const productList = await Product.find().populate('catagory')
+    let filter = {}
+    if(req.query.catagories) //http://localhost:8000/product?catagories=222,5245
+    {
+        filter = {catagory: req.query.catagories.split(',')}
+    }
+    const productList = await Product.find(filter).populate('catagory')
 
     if(!productList){
         return res.status(400).json({success: false, message: "fetching productList is failed !!"})
@@ -64,4 +69,61 @@ route.delete('/:id', async(req,res) => {
     }
     return res.status(200).send({success: true, message: "successfully deleted !!"});
 })
+
+route.put('/:id', async(req,res) => {
+    if(!mongoose.isValidObjectId(req.params.id)){
+        return res.status(400).json({success: false, message: "Invalid Product Id !!"})
+    }
+
+    let catagory = await Catagory.findById(req.body.catagory);
+    if(!catagory && catagory != null){
+        return res.status(400).json({success: false, message: "Invalid Catagory !!"})
+    }
+    
+    let product = await Product.findByIdAndUpdate(
+        req.params.id,
+        {
+            name: req.body.name,
+            description: req.body.description,
+            richDescription: req.body.richDescription,
+            images: req.body.images,
+            brand: req.body.brand,
+            price: req.body.price,
+            catagory: req.body.catagory,
+            countInStock: req.body.countInStock,
+            rating: req.body.rating,
+            isFeatured: req.body.isFeatured,
+            dateCreated: req.body.dateCreated
+        },
+        {new: true}
+    )
+
+    if(!product){
+        return res.status(400).json({success: false, message: "product update failed !!"})
+    }
+
+    return res.status(200).send(product);
+})
+
+route.get('/get/count', async(req,res) => {
+    const productCount = await Product.countDocuments()
+
+    if(!productCount){
+        return res.status(500).send({success: false})
+    }
+
+    return res.status(200).send({productCount: productCount})
+})
+
+route.get('/get/featured/:count', async(req,res) => {
+    const count = req.params.count ? req.params.count : 0
+    const product = await Product.find({isFeatured: true}).limit(count)
+
+    if(!product){
+        return res.status(500).send({success: false})
+    }
+
+    return res.status(200).send(product)
+})
+
 export default route
